5f45e1b101e125907f6bfeee6c0554416a3cf257
XxxxxMing <windlessg@gmail.com>
2019-10-29 15:05:47 +0800

    去除 max-lines-per-function

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ module.exports = {
     "lines-between-class-members": ["error", "always"],
     "max-depth": ["error", 4],
     "max-len": ["error", 120],
-    "max-lines-per-function": "error",
+    "max-lines-per-function": "off",
     "new-cap": "error",
     "new-parens": "error",
     "no-multiple-empty-lines": ["error", {
