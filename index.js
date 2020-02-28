e0fd925f00ba06c6c6fc79dfba491ea4241d43e5
Finian <g@affount.com>
2020-02-28 19:11:41 +0800

    修复 `no-use-before-define` 配置错误问题

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ module.exports = {
     'init-declarations': 'off',
     'no-shadow': 'off',
     'no-undefined': 'off',
-    'no-use-before-define': 'nofunc',
+    'no-use-before-define': ['error', 'nofunc'],
 
     // --- Node.js and CommonJS
     'callback-return': 'off',
